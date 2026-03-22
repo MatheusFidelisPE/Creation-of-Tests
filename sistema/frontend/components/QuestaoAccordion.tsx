947b5283ac65c217cd09a1cc55860ead0dbe86be
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2026-03-22 13:33:35 -0300

    Remoção do botão 'gerenciar alternativas' pois não era mais necessário

--- sistema/frontend/components/QuestaoAccordion.tsx
+++ sistema/frontend/components/QuestaoAccordion.tsx
@@ -32,9 +32,6 @@ export default function QuestaoAccordion({ questao }: QuestaoAccordionProps) {
               </li>
             ))}
           </ul>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Gerenciar Alternativas
-          </button>
         </div>
       )}
     </div>
